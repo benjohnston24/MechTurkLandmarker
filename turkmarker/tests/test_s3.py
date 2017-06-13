08f1535be297867170d055d96580b8c19868acfa
Ben Johnston <bjohnston@neomailbox.net>
2017-06-13 20:27:37 +1000

    aws S3 tests complete

--- turkmarker/tests/test_s3.py
+++ turkmarker/tests/test_s3.py
@@ -10,7 +10,7 @@ import unittest
 import boto3
 from unittest import skip
 from unittest.mock import patch, MagicMock 
-from turkmarker.aws.s3 import AWSS3 
+from turkmarker.aws.s3 import AWSS3, CONTENT_TYPES 
 
 
 class TestS3API(unittest.TestCase):
@@ -22,9 +22,12 @@ class TestS3API(unittest.TestCase):
                 'BUCKET_NAME': 'bucket_name',
                 'ACL': 'public-read',
                 },
-            'KEYS':{
+            'KEYS': {
                 'AWS-ID': '123456',
                 'AWS-KEY': '5678',
+                },
+            'LANDMARK-DETAILS': {
+                'STATIC_FOLDER': 'static_folder',
                 }
             }
 
@@ -81,7 +84,6 @@ class TestS3API(unittest.TestCase):
         self.obj.s3.create_bucket = MagicMock()
         self.obj.create_bucket()
 
-        self.assertEqual(boto_mock.call_count, 1)
         self.obj.s3.create_bucket.assert_called_with(
             ACL=self.config['AWS-S3']['ACL'],
             Bucket=self.config['AWS-S3']['BUCKET_NAME'],
@@ -90,25 +92,102 @@ class TestS3API(unittest.TestCase):
                  }
             )
 
-#    @patch('builtins.print')
-    @patch('boto3.client', autospec=boto3.client)
-    def test_create_bucket_not_connected_bucket_exists(self, boto_mock):
+    @patch('builtins.print')
+    def test_create_bucket_not_connected_bucket_exists(self, print_mock):
         """Test creating a bucket on AWS - is not connected - no existing buckets"""
 
+        self.obj.connect = MagicMock()
         self.obj.s3 = MagicMock()
-        self.obj.s3.list_buckets = MagicMock(side_effect= lambda :{
-                'Buckets': [self.config['AWS-S3']['BUCKET_NAME']]})
-        self.obj.s3.create_bucket = MagicMock()
-        import pdb;pdb.set_trace()
-        self.obj.create_bucket()
+        self.obj.s3.list_buckets  = MagicMock(side_effect=lambda :{
+            'Buckets': [{'Name':self.config['AWS-S3']['BUCKET_NAME']}]})
+        bucket = self.obj.create_bucket()
 
-#        print_mock.assert_called_with('%s Bucket exists' % 
-#            self.config['AWS-S3']['BUCKET_NAME'])
-        self.assertEqual(boto_mock.call_count, 1)
-        self.obj.s3.create_bucket.assert_called_with(
-            ACL=self.config['AWS-S3']['ACL'],
-            Bucket=self.config['AWS-S3']['BUCKET_NAME'],
-            CreateBucketConfiguration={
-                 'LocationConstraint': self.config['AWS-S3']['REGION'],
-                 }
-            )
+        self.assertTrue(self.obj.connect.called)
+        self.assertEqual(bucket, {'Name': 'bucket_name'})
+        print_mock.assert_called_with('%s Bucket exists' %
+            self.config['AWS-S3']['BUCKET_NAME'])
+
+    @patch('builtins.print')
+    @patch('boto3.client')
+    def test_upload_file_connected(self, boto_mock, print_mock):
+        """Test uploading files - is already connected"""
+
+        example_files = [
+            'test.html',
+            'test.css',
+            'test.jpg',
+            'test.jpeg',
+            'test.png',
+            'test.svg',
+            'test.js',
+            'test.json',
+            'test.eot',
+            'test.woff',
+            'test.ttf',
+        ]
+
+        def produce_files(*args, **kwargs):
+            for filename in example_files:
+                yield filename
+
+        self.obj.connect()
+        self.obj.s3.upload_file = MagicMock()
+
+        with patch('os.listdir', side_effect=produce_files) as\
+            listdir_mock:
+        
+            self.obj.upload_files()
+
+            for filename in example_files:
+                _, ext = os.path.splitext(filename)
+                self.obj.s3.upload_file.assert_any_call(
+                    os.path.join(self.config['LANDMARK-DETAILS']['STATIC_FOLDER'],
+                        filename),
+                    self.config['AWS-S3']['BUCKET_NAME'],
+                    os.path.join(self.config['LANDMARK-DETAILS']['STATIC_FOLDER'],
+                        filename),
+                    ExtraArgs={
+                        'ContentType': CONTENT_TYPES[ext],
+                        'ACL': self.config['AWS-S3']['ACL'],
+                    }
+                )
+                print_mock.assert_any_call(
+                    'Uploading: %s' %
+                    os.path.join(self.config['LANDMARK-DETAILS']['STATIC_FOLDER'],
+                        filename)
+                )
+
+            for filename in ['index.html', 'error.html', 'protocol.html']:
+                _, ext = os.path.splitext(filename)
+                self.obj.s3.upload_file.assert_any_call(
+                    filename,
+                    self.config['AWS-S3']['BUCKET_NAME'],
+                    filename,
+                    ExtraArgs={
+                        'ContentType': CONTENT_TYPES[ext],
+                        'ACL': self.config['AWS-S3']['ACL'],
+                    }
+                )
+                print_mock.assert_any_call(
+                    'Uploading: %s' % filename)
+
+
+    @patch('builtins.print')
+    @patch('boto3.client')
+    def test_upload_file_not_connected(self, boto_mock, print_mock):
+        """Test the connect function is called if not otherwise connected""" 
+
+        self.obj.connect = MagicMock()
+        self.obj.s3 = MagicMock()
+        self.obj.s3.upload_file = MagicMock()
+
+        def produce_files(*args, **kwargs):
+            for filename in ['test.css']:
+                yield filename
+
+        with patch('os.listdir', side_effect=produce_files) as\
+            listdir_mock:
+        
+            self.obj.upload_files()
+
+        self.assertTrue(self.obj.connect.called)
